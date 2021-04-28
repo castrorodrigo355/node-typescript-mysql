4d337993391201d577ff1db8c1dca23343fe633b
castrorodrigo355 <castrorodrigo355@gmail.com>
2021-04-28 12:47:22 -0300

    node-ts-no-ts

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { validateJWT } from "../middlewares/jwt.middleware";
+// import { validateJWT } from "../middlewares/jwt.middleware";
 import { createEvent, getEvents } from "../controllers/event.controller";
 
 const eventRouter = Router();
 
 eventRouter.get("/", getEvents);
-eventRouter.post("/", validateJWT, createEvent);
+eventRouter.post(
+	"/",
+	// validateJWT
+	createEvent,
+);
 // eventRouter.get("/:id", getUserById);
 // eventRouter.delete("/:id", deleteUser);
 // eventRouter.put("/:id", updateUser);
